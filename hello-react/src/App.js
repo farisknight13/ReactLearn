1f4d3f70715821cc2a7814636adafa82950871a5
farisknight13 <knightfaris13@gmail.com>
2021-06-15 19:41:15 +0700

    update count

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,9 +2,16 @@ import React, { useState } from "react";
 
 export default ()=> {
 
+  let [count , setCount] = useState(0);
+
   return (
     <div>
       <h1>Farisknight</h1>
+      <h2>Count : {count}</h2>
+      <button
+      onClick = { () => {
+        setCount(count+1);
+      }}>add</button>
     </div>
   );
 };
